58d94b224cf12c4d1fa5b8b9a0a3047c723b9126
stevensa <andrew.stevens@infineon.com>
2020-05-08 12:47:55 +0200

    Add: packed weights test

---
+++ tensorflow/lite/micro/examples/packed_weights/hello_world_4bit_q.h
@@ -0,0 +1,10 @@
+/**
+* 4-Bit weight 'hello world' sine model
+*/
+
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+extern const uint8_t hello_world_4bit_q[];
+extern const size_t hello_world_4bit_q_len;
