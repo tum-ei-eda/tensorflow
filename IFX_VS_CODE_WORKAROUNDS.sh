62bdceb6f77601326e464628a96f55a6388a4038
stevensa <andrew.stevens@infineon.com>
2020-06-15 19:23:13 +0200

    Fix: update workarounds VS-code

--- IFX_VS_CODE_WORKAROUNDS.sh
+++ IFX_VS_CODE_WORKAROUNDS.sh
@@ -2,5 +2,6 @@
 # Some minor hacks to suppress breakages in bazel scripts
 # that poison VS-code bazel target discovery
 cp third_party/toolchains/cpus/arm/cc_config.bzl.tpl third_party/toolchains/cpus/arm/cc_config.bzl
+cp third_party/toolchains/embedded/arm-linux/cc_config.bzl.tpl third_party/toolchains/embedded/arm-linux/cc_config.bzl
 patch --strip=1  < VS_code_bazel_query.patch
 echo >  tensorflow/lite/micro/tools/make/downloads/flatbuffers/go/BUILD.bazel
