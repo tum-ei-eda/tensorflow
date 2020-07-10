74810dac0a5be82e7a922e17804329bed20c88e8
Andrew Stevens <wackston@googlemail.com>
2020-07-10 17:42:37 +0200

    Added: test more build variants of tflite(u)

--- ifx_tests/IFX_TEST_010052_tflite_u_host_32bit.sh
+++ ifx_tests/IFX_TEST_010052_tflite_u_host_32bit.sh
@@ -4,6 +4,6 @@ set -e
 cd `dirname "$BASH_SOURCE"`
 
 # Native host build of hello_world, execute and check signals test passed
-make -C .. clean
-make -C .. -j 4  test_executables
+make -C .. TARGET_ARCH=i386 clean
+make -C .. -j 4  TARGET_ARCH=i386 test_executables
 make -C .. test
--- ifx_tests/IFX_TEST_010070_tflite_u_ifx_riscv.sh
+++ ifx_tests/IFX_TEST_010070_tflite_u_ifx_riscv.sh
@@ -5,6 +5,7 @@ cd `dirname "$BASH_SOURCE"`
 
 
 # Native host build of micro_speech, execute and check signals test passed
+make -C .. TARGET=ifx_riscv32_mcu clean
 make  -C .. -j 4 TARGET=ifx_riscv32_mcu test_executables
 make  -C .. TARGET=ifx_riscv32_mcu test
 
