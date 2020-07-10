9c41d3c3f8a545749187a5629c1856b7ee8c79d1
Andrew Stevens <wackston@googlemail.com>
2020-07-11 01:00:23 +0200

    Fix: typo in hostnative 32-bit tflite(u) test

--- ifx_tests/IFX_TEST_010052_tflite_u_host_32bit.sh
+++ ifx_tests/IFX_TEST_010052_tflite_u_host_32bit.sh
@@ -6,4 +6,4 @@ cd `dirname "$BASH_SOURCE"`
 # Native host build of hello_world, execute and check signals test passed
 make -C .. TARGET_ARCH=i386 clean
 make -C .. -j 4  TARGET_ARCH=i386 test_executables
-make -C .. test
+make -C .. TARGET_ARCH=i386 test
