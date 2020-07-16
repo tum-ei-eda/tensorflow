7d1e4c588cf31dc820f8cf3a52bd7e49a06bc6b1
Kruse Jakob (IFAG DES SDF SCS SEM) <Jakob.Kruse@infineon.com>
2020-07-16 13:54:04 +0200

    Moved packed weights operation  for conv to micro kernels folder

--- tensorflow/lite/micro/kernels/conv.cc
+++ tensorflow/lite/micro/kernels/conv.cc
@@ -13,6 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <tensorflow/lite/micro/kernels/conv_packed_ops.h>
 #include "tensorflow/lite/kernels/internal/reference/conv.h"
 
 #include "tensorflow/lite/c/builtin_op_data.h"
@@ -20,7 +21,6 @@ limitations under the License.
 #include "tensorflow/lite/kernels/internal/common.h"
 #include "tensorflow/lite/kernels/internal/quantization_util.h"
 #include "tensorflow/lite/kernels/internal/reference/integer_ops/conv.h"
-#include "tensorflow/lite/kernels/internal/reference/conv_packed_ops.h"
 #include "tensorflow/lite/kernels/internal/tensor_ctypes.h"
 #include "tensorflow/lite/kernels/kernel_util.h"
 #include "tensorflow/lite/kernels/padding.h"
