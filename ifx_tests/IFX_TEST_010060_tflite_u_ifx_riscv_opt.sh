b37a8b4004a999aade7a6ba7fa1002076d4faf1f
Andrew Stevens <wackston@googlemail.com>
2020-08-07 23:11:51 +0200

    Tidy: execute tfllite(u) RISC-V tests in parallel

--- ifx_tests/IFX_TEST_010060_tflite_u_ifx_riscv_opt.sh
+++ ifx_tests/IFX_TEST_010060_tflite_u_ifx_riscv_opt.sh
@@ -7,5 +7,5 @@ cd `dirname "$BASH_SOURCE"`
 # Native host build of micro_speech, execute and check signals test passed
 make -C .. TARGET=ifx_riscv32_mcu clean
 make  -C .. -j 4 TARGET=ifx_riscv32_mcu TAGS=portable_optimized test_executables
-make  -C .. TARGET=ifx_riscv32_mcu TAGS=portable_optimized test
+make  -C .. -j 4 TARGET=ifx_riscv32_mcu TAGS=portable_optimized test
 
