8e36550edbc8c67bd6c01599ae07ce0f3cca78b7
Moshak <ga46peb@prakt16.regent.e-technik.tu-muenchen.de>
2020-08-22 20:07:21 +0200

    remove unused code

--- tensorflow/lite/micro/kernels/cmsis-nn-riscv/conv.cc
+++ tensorflow/lite/micro/kernels/cmsis-nn-riscv/conv.cc
@@ -114,7 +114,6 @@ void* Init(TfLiteContext* context, const char* buffer, size_t length) {
 }
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
-#if defined(RISCV)
   OpData data;
   int32_t buf_size = 0;
 
@@ -179,7 +178,6 @@ TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
   } else {
     *buffer_idx = -1;
   }
-#endif
   return kTfLiteOk;
 }
 
@@ -239,7 +237,6 @@ TfLiteStatus EvalQuantizedPerChannel(
   quant_params.multiplier = data->per_channel_output_multiplier;
   quant_params.shift = data->per_channel_output_shift;
 
-#if defined(RISCV)
   RuntimeShape filter_shape = GetTensorShape(filter);
   RuntimeShape input_shape = GetTensorShape(input);
   RuntimeShape output_shape = GetTensorShape(output);
@@ -319,31 +316,6 @@ TfLiteStatus EvalQuantizedPerChannel(
     return kTfLiteError;
   }
 
-#else
-#pragma message( \
-    "riscv-NN optimization for conv not available for this target. Using reference kernel.")
-
-  ConvParams op_params;
-  op_params.input_offset = -input->params.zero_point;
-  op_params.output_offset = output->params.zero_point;
-  op_params.stride_height = params->stride_height;
-  op_params.stride_width = params->stride_width;
-  op_params.dilation_height_factor = params->dilation_height_factor;
-  op_params.dilation_width_factor = params->dilation_width_factor;
-  op_params.padding_values.height = data->padding.height;
-  op_params.padding_values.width = data->padding.width;
-  op_params.quantized_activation_min = data->output_activation_min;
-  op_params.quantized_activation_max = data->output_activation_max;
-
-  reference_integer_ops::ConvPerChannel(
-      op_params, data->per_channel_output_multiplier,
-      data->per_channel_output_shift, GetTensorShape(input),
-      GetTensorData<int8>(input), GetTensorShape(filter),
-      GetTensorData<int8>(filter), GetTensorShape(bias),
-      GetTensorData<int32>(bias), GetTensorShape(output),
-      GetTensorData<int8>(output));
-
-#endif
   return kTfLiteOk;
 }
 
