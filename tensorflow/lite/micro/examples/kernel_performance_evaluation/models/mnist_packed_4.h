78c3c0ac435479d2bafee9ce1f5373154d5968af
Kruse Jakob (IFAG DES SDF SCS SEM) <Jakob.Kruse@infineon.com>
2020-09-16 15:17:44 +0200

    Added tf models for MNIST performance tests

---
+++ tensorflow/lite/micro/examples/kernel_performance_evaluation/models/mnist_packed_4.h
@@ -0,0 +1,10 @@
+/**
+* 4-Bit weight 'mnist' model
+*/
+
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+extern const uint8_t mnist_packed_4_data[];
+extern const size_t mnist_packed_4_len;
